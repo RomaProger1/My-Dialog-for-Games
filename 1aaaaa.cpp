53130a4a61e0a64656cbd9ead58b81beda3aaa2b
RomaProger1 <101552538+RomaProger1@users.noreply.github.com>
2022-04-10 00:48:12 +0700

    Мой первый вариант диалогов для игры

---
+++ 1aaaaa.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+ void dialog1() {
+
+	 cout << "1.Подойти к стражнику у ворот\n2.Уйти" << endl;
+	int Name = 0;
+	int Oplata;
+	cin >> Name;
+
+	string a = "{Игрок}";
+	string b = "{Стражник} ";
+
+
+	switch (Name)
+	{
+	case 1:
+		cout << a << "Приветствую" << endl;
+		cout << b << "Приветствую вас странник " << endl;
+		cout << b << "Плата за проход 25 золотых " << endl;
+		cout << "1.Оплатить\n2.Уйти" << endl;
+		cin >> Oplata;
+		if (Oplata == 1)
+		{
+			cout << " - 25 золотых, проходите ";
+
+		}
+		else if (Oplata == 2)
+		{
+			while (Oplata == 2)
+			{
+				dialog1();
+			}
+		}
+		break;
+	case 2:
+		cout << "Вы прошли мимо" << endl;
+		break;
+	default:
+		while (Name != true)
+		{
+			cout << "Нет тaкого варианта ! :)";
+			dialog1();
+		}
+		break;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	dialog1();
+
+	return 0;
+}
